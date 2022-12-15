a7f4539eabf0907b721268b493ed27b43c842086
ZhanPascal <102191853+ZhanPascal@users.noreply.github.com>
2022-12-15 13:21:15 +0100

    Add files via upload
    Trigger pour les tables

---
+++ Code/import IMDB to POSTGRES/python/sql/TRIGGER.sql
@@ -0,0 +1,206 @@
+CREATE FUNCTION tonstCheck() --Check si les tconst sont présents dans la table titlebasics
+    RETURNS TRIGGER AS
+$$
+    DECLARE
+        t titlebasics.tconst%TYPE;
+    BEGIN
+        SELECT tconst INTO t
+            FROM titlebasics
+            WHERE tconst = NEW.tconst;
+        IF NOT FOUND THEN
+            RETURN NULL;
+        END IF;
+        RETURN NEW;
+    END;
+
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION nconstCheck() --Check si les nconst sont présents dans la table namebasics
+    RETURNS TRIGGER AS
+$$
+    DECLARE
+        n namebasics.nconst%TYPE;
+    BEGIN
+        SELECT nconst INTO n
+            FROM namebasics
+            WHERE nconst = NEW.nconst;
+        IF NOT FOUND THEN
+            RETURN NULL;
+        END IF;
+        RETURN NEW;
+    END;
+
+$$ LANGUAGE plpgsql;
+
+CREATE FUNCTION parentTonstCheck() --Check si les parentTconst sont présents dans la table titlebasics
+    RETURNS TRIGGER AS
+$$
+    DECLARE
+        t titlebasics.tconst%TYPE;
+    BEGIN
+        SELECT tconst INTO t
+            FROM titlebasics
+            WHERE tconst = NEW.parenttconst;
+        IF NOT FOUND THEN
+            RETURN NULL;
+        END IF;
+        RETURN NEW;
+    END;
+
+$$ LANGUAGE plpgsql;
+
+
+--TRIGGER principals
+DROP TRIGGER IF EXISTS principals_check_nconst on titleprincipals;
+CREATE TRIGGER principals_check_nconst
+    BEFORE INSERT ON titleprincipals
+    FOR EACH row
+    EXECUTE PROCEDURE nconstCheck();
+
+DROP TRIGGER IF EXISTS principals_check_tconst on titleprincipals;
+CREATE TRIGGER principals_check_tconst
+    BEFORE INSERT ON titleprincipals
+    FOR EACH row
+    EXECUTE PROCEDURE tconstCheck();
+
+
+--TRIGGER episode
+DROP TRIGGER IF EXISTS episode_check_tconst on titleepisode;
+CREATE TRIGGER episode_check_tconst
+    BEFORE INSERT ON titleepisode
+    FOR EACH row
+    EXECUTE PROCEDURE tconstCheck();
+
+DROP TRIGGER IF EXISTS episode_check_parenttconst on titleepisode;
+CREATE TRIGGER episode_check_parenttconst
+    BEFORE INSERT ON titleepisode
+    FOR EACH row
+    EXECUTE PROCEDURE parentTonstCheck();
+
+
+
+--TRIGGER crew
+DROP TRIGGER IF EXISTS crew_check_tconst on titlecrew;
+CREATE TRIGGER crew_check_tconst
+    BEFORE INSERT ON titlecrew
+    FOR EACH row
+    EXECUTE PROCEDURE tconstCheck();
+
+
+--TRIGGER ratings
+DROP TRIGGER IF EXISTS ratings_check_tconst on titleratings;
+CREATE TRIGGER ratings_check_tconst
+    BEFORE INSERT ON titleratings
+    FOR EACH row
+    EXECUTE PROCEDURE tconstCheck();
+
+
+
+
+-- TRIGGER known title table namebasics
+CREATE or replace FUNCTION knownTitleCheck() --Check si les tconst dans l'array sont présents dans la table titlebasics
+    RETURNS TRIGGER AS
+$$
+    DECLARE
+        t titlebasics.tconst%TYPE;
+		tconstachecker titlebasics.tconst%TYPE;
+    BEGIN
+		IF NEW.knownfortitles is NULL THEN
+			return NEW;
+		END IF;
+		
+		FOR tconstachecker IN SELECT unnest(NEW.knownfortitles)
+		LOOP
+			SELECT tconst INTO t
+				FROM titlebasics
+				WHERE tconst = tconstachecker;
+			IF NOT FOUND THEN
+				RETURN NULL;
+        	END IF;
+		END LOOP;
+        RETURN NEW;
+    END;
+
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS namebasics_check_knownfortitle on namebasics;
+CREATE TRIGGER namebasics_check_knownfortitle
+    BEFORE INSERT ON namebasics
+    FOR EACH row
+    EXECUTE PROCEDURE knownTitleCheck();
+
+
+
+--array nconst de crew
+CREATE or replace FUNCTION arrayNconstCheck() --Check si les nconst dans l'array sont présents dans la table namebasics
+    RETURNS TRIGGER AS
+$$
+    DECLARE
+        n namebasics.nconst%TYPE;
+		nconstachecker namebasics.nconst%TYPE;
+
+    BEGIN
+		IF NEW.directors is NULL and NEW.writers is NULL THEN
+			return NEW;
+		END IF;
+		
+  IF NEW.directors is not null THEN
+		 FOR nconstachecker IN SELECT unnest(NEW.directors)
+		 LOOP
+			 SELECT nconst INTO n
+				 FROM namebasics
+				 WHERE nconst = nconstachecker;
+			 IF NOT FOUND THEN
+				 RETURN NULL;
+    	END IF;
+		  END LOOP;
+  END IF ;
+
+  IF NEW.writers is not null THEN
+		 FOR nconstachecker IN SELECT unnest(NEW.writers)
+		 LOOP
+			 SELECT nconst INTO n
+				 FROM namebasics
+				 WHERE nconst = nconstachecker;
+			 IF NOT FOUND THEN
+				 RETURN NULL;
+    	END IF;
+		  END LOOP;
+  END IF ; 
+        RETURN NEW;
+    END;
+
+$$ LANGUAGE plpgsql;
+
+DROP TRIGGER IF EXISTS crew_ckeck_array on titlecrew;
+CREATE TRIGGER crew_ckeck_array
+    BEFORE INSERT ON titlecrew
+    FOR EACH row
+    EXECUTE PROCEDURE arrayNconstCheck();
+
+
+
+--TRIGGER Function akas
+CREATE FUNCTION titleIdCheck() --Check si les tconst sont présents dans la table titlebasics
+    RETURNS TRIGGER AS
+$$
+    DECLARE
+        t titlebasics.tconst%TYPE;
+    BEGIN
+        SELECT tconst INTO t
+            FROM titlebasics
+            WHERE tconst = NEW.titleid;
+        IF NOT FOUND THEN
+            RETURN NULL;
+        END IF;
+        RETURN NEW;
+    END;
+
+$$ LANGUAGE plpgsql;
+
+--TRIGGER akas
+DROP TRIGGER IF EXISTS akas_check_tconst on titleakas;
+CREATE TRIGGER akas_check_tconst
+    BEFORE INSERT ON titleakas
+    FOR EACH row
+    EXECUTE PROCEDURE titleIdCheck();
